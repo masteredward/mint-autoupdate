e332299374f5c8166103b7176c905d87f28f697d
Eduardo Medeiros Silva <edu.medeiros.info@gmail.com>
2018-05-15 23:08:38 -0300

    Added Install Script

---
+++ install.sh
@@ -0,0 +1,19 @@
+INSTALL_DIR=/opt/autoupdate
+SYSTEMD_DIR=/etc/systemd/system
+
+if [ $EUID != 0 ]
+then
+    echo "Not running as root!!! Aborted!"
+    exit
+fi
+
+mkdir $INSTALL_DIR
+cp autoupdate.sh $INSTALL_DIR
+touch $INSTALL_DIR/autoupdate.log
+chown root. $INSTALL_DIR/autoupdate.*
+chmod 744 $INSTALL_DIR/autoupdate.sh
+cp autoupdate.service $SYSTEMD_DIR
+chown root. $SYSTEMD_DIR/autoupdate.service
+systemctl enable autoupdate.service
+systemctl start autoupdate.service
+echo "Installed!!!"
